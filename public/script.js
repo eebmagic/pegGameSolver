df75c1410e226776b2419a011143900aeb567869
E Bolton <eebmagic@gmail.com>
2021-08-14 01:58:31 -0400

    recSolve working

---
+++ public/script.js
@@ -0,0 +1,219 @@
+function processPegs() {
+  let values = Array(15).fill(false);
+  let pegs = document.getElementsByClassName('peg');
+  Array.from(pegs).forEach(function(p, ind){
+    if (p.dataset.clicked == "true") {
+      values[ind] = true;
+    }
+  });
+
+  let out = [
+    [values[0]],
+    [values[1], values[2]],
+    [values[3], values[4], values[5]],
+    [values[6], values[7], values[8], values[9]],
+    [values[10], values[11], values[12], values[13], values[14]],
+  ]
+
+  return out;
+}
+
+function validMove(a, b) {
+  /**
+   *  Where a and b are two coordinates (x, y).
+   *  Checks that peg at point a can possibly jump point b.
+   * 
+   *  Return the new position of peg a after jumping peg b.
+   *  Return false if jump not possible.
+   */
+  // console.log(`a: ${a}`);
+  // console.log(`b: ${b}`);
+  if (a == b) return false;
+  if (Math.abs(a[0] - b[0]) > 1) return false;
+  if (Math.abs(a[1] - b[1]) > 1) return false;
+  if (Math.abs(a[0] - b[0]) + Math.abs(a[0] - b[0]) > 2) return false;
+
+  let out = "default";
+  if (a[0] == b[0]) {
+    if (Math.abs(a[1] - b[1]) == 1) {
+      if (a[1] > b[1]) {
+        // Left
+        out = [a[0], b[1]-1];
+      } else {
+        // Right
+        out = [a[0], b[1]+1];
+      }
+    }
+  } else {
+    // console.log('WILL BE DIAGONAL JUMP')
+    if (b[1] == a[1]) {
+      if (b[0] > a[0]) {
+        // Down left
+        // console.log("Down left");
+        out = [b[0]+1, b[1]];
+      } else {
+        // Up right
+        // console.log("Up right");
+        out = [b[0]-1, b[1]];
+      }
+    } else if (b[1] == a[1] + 1 && b[0] == a[0] + 1) {
+      // Down right
+      // console.log("Down right");
+      out = [b[0]+1, b[1]+1];
+    } else if (b[1] + 1 == a[1]) {
+      // Up left
+      // console.log("Up left");
+      out = [b[0]-1, b[1]-1];
+    }
+  }
+
+  // Check output a valid point
+  if (out[0] < 0 || out[0] > 4) {
+    return false;
+  }
+  if (out[1] > out[0] || out[1] < 0 || out[1] > 4) {
+    return false;
+  }
+  return out;
+}
+
+function makeBoardCopy(boardMatrix) {
+  /**
+   *  Make a deep copy of a board matrix.
+   */
+  let out = [];
+  boardMatrix.forEach(row => {
+    out.push(row.slice());
+  })
+  return out
+}
+
+function makeBoard(triboardArr) {
+  /**
+   * Board object
+   */
+  return {
+    board: makeBoardCopy(triboardArr),
+    points: [
+      [0, 0], [1, 0], [1, 1],
+      [2, 0], [2, 1], [2, 2],
+      [3, 0], [3, 1], [3, 2],
+      [3, 3], [4, 0], [4, 1],
+      [4, 2], [4, 3], [4, 4]
+    ],
+
+    getMoves: function() {
+      /**
+       *  Iterate over all peg pairs and check for valid moves.
+       *  Return a list of all (a, b) peg pairs (where a can jump b).
+       */
+      let out = [];
+      for (let aInd = 0; aInd < this.points.length; aInd++) {
+        for (let bInd = 0; bInd < this.points.length; bInd++) {
+          if (aInd != bInd) {
+            let a = this.points[aInd];
+            let b = this.points[bInd];
+            if (this.board[a[0]][a[1]] == true && this.board[b[0]][b[1]] == true) {
+              let c = validMove(a, b);
+              if (c != false && this.board[c[0]][c[1]] == false) {
+                out.push([a, b]);
+              }
+            }
+          }
+        }
+      }
+      return out;
+    },
+
+    getPegCount: function() {
+      let total = 0;
+      this.board.forEach((arr) => {
+        arr.forEach((value) => {
+          if (value == true) {
+            total += 1;
+          }
+        });
+      });
+      return total;
+    },
+
+    makeMove: function(a, b) {
+      /**
+       *  Check valid move and perform corresponding flips on board.
+       */
+      let c = validMove(a, b);
+      if (c == false) {
+        console.log(`Invalid move: ${a} -> ${b}`);
+        return;
+      }
+
+      // Check c is available
+      let aValid = this.board[a[0]][a[1]] == true;
+      let bValid = this.board[b[0]][b[1]] == true;
+      let cValid = this.board[c[0]][c[1]] == false;
+      if (aValid && bValid && cValid) {
+        this.board[a[0]][a[1]] = false;
+        this.board[b[0]][b[1]] = false;
+        this.board[c[0]][c[1]] = true;
+        return;
+      } else {
+        console.log(`Jump not possible ${a} -> ${b} | ${c}`);
+        return;
+      }
+    }
+  }
+}
+
+function evalMatrix(boardMatrix) {
+  /**
+   *  Count how many true values in a board matrix.
+   *  Return int total true values.
+   */
+  let total = 0;
+  boardMatrix.forEach(row => {
+    row.forEach(value => {
+      if (value) total += 1;
+    });
+  });
+  return total;
+}
+
+function recSolve(board, path) {
+  /**
+   *  Recursive DFS implementation to find longest path.
+   *  Longest path in this case will involve removing the
+   *  most number of pegs possible (each step in path being a removal).
+   */
+  let moves = board.getMoves();
+  if (moves.length == 0) {
+    return path;
+  }
+
+  let bestPath = path.slice();
+  for (let i = 0; i < moves.length; i++) {
+    let move = moves[i];
+    let childBoard = makeBoard(board.board);
+    childBoard.makeMove(move[0], move[1]);
+    let childPath = path.slice();
+    childPath.push([move, childBoard.board]);
+
+    let p = recSolve(childBoard, childPath);
+    if (p.length > bestPath.length) {
+      bestPath = p;
+    }
+  }
+  return bestPath;
+}
+
+function main() {
+  console.log('STARTING MAIN FUNC!!!');
+  let pegs = processPegs();
+
+  let startBoard = makeBoard(pegs);
+  let nextMove = recSolve(startBoard, [[null, startBoard.board]]);
+  console.log("Next move:");
+  console.log(nextMove);
+  let finalTotal = evalMatrix(nextMove[nextMove.length-1][1]);
+  console.log(`FINAL BOARD HAS ${finalTotal} PEGS`);
+}
+
